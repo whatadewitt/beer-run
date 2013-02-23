807deac291a8465b8f70a9a57b2bd0ad4b36778e
FKabir <fkabir@herald.ca>
2013-02-23 15:01:18 -0400

    pushing FBauth.js, passport-facebook module

---
+++ FBAuth.js
@@ -0,0 +1,49 @@
+module.exports = function(app) {
+  'use strict';
+
+  var passport = require('passport'), FacebookStrategy = require('passport-facebook').Strategy;
+  var redis = require("redis");
+  var client = redis.createClient();
+
+  client.on("error", function (err) {
+          console.log("Error " + err);
+      });
+
+  passport.use(new FacebookStrategy({
+      clientID: 347355238716033,
+      clientSecret: 5552d27c383d1f73ca957cd32b231a3a,
+      callbackURL: "http://localhost:3000/auth/facebook/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+      client.set('User: ' + profile.id, JSON.stringify(profile), function(err, response) {
+
+        if (err) {
+          done(err);
+        } else {
+          client.set('User: ' + profile.id + ':AccessToken', accessToken, function(err, response) {
+            if (err) {
+              done(err);
+            } else {
+              return done(null, profile.id);  
+            }
+            
+          });  
+        }      
+      });
+    }
+  ));
+
+  passport.serializeUser(function(user, done) {
+      done(null, user.id);
+    });
+
+  passport.deserializeUser(function(id, done) {
+    client.get('User: ' + id, function(err, reply) {
+      done(null, reply);
+    });
+  });
+
+  app.get('/auth/facebook', passport.authenticate('facebook'));
+
+  app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/' }));
+}
