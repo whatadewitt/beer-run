30afa6e391361cd87b94befc983fda9965bb7df6
Tyler Smith <tyler.dean.smith@gmail.com>
2013-02-23 13:37:48 -0400

    UI Updates

--- app.js
+++ app.js
@@ -20,10 +20,9 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser('your secret here'));
+  app.use(express.static(path.join(__dirname, 'public')));
   app.use(express.session());
   app.use(app.router);
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
-  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
