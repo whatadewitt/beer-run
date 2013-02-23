3d498f2f00045501d76ecf57b5e894be3799d1d1
Jamie Stackhouse <contin673@gmail.com>
2013-02-23 18:52:36 -0400

    Updated Auth to redirect to route.

--- FBAuth.js
+++ FBAuth.js
@@ -44,7 +44,7 @@ module.exports = function(app) {
   app.get('/auth/facebook', passport.authenticate('facebook'));
 
   app.get('/auth/facebook/callback', passport.authenticate('facebook', 
-    { successRedirect: '/createRun', failureRedirect: '/fail' })
+    { successRedirect: '/create', failureRedirect: '/' })
   );
 
   app.get('/logout', function(req, res) {
