e96202c3cb727dcf893cf0618b19caa60c9b7cf2
Jamie Stackhouse <contin673@gmail.com>
2013-02-23 18:00:13 -0400

    API Work.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,5 @@
 module.exports = (function() {
+    var async = require('async');
     var runStore = new require('../../runstore');
 
     function createRun(req, res) {
@@ -11,24 +12,46 @@ module.exports = (function() {
         if (err) {
           res.send({
             code: 400,
-            message: err,
-          });
+            message: err
+          }, 400);
         } else {
           res.send({
             code: 200,
             message: 'Run successfully created.',
+            data: {
+              //facebook data
+            },
             runId: runId
-          });    
+          }, 200);    
         }
       });
     }
 
     function finishRun(req, res) {
-
+      runStore.finishRun({
+        fb_id: req.user.id,
+        r_id: req.params.id
+      }, function(err, success) {
+        if(err && !success) {
+          res.send({
+            code: 400,
+            message: err
+          }, 400);
+        } else {
+          res.send({
+            code: 200,
+            message: 'Beer has been got!'
+          }, 200);
+        }
+      })
     }
 
     function addItem(req, res) {
+      async.each(req.body.items)
 
+      runStore.addItem({
+        r_id: req.params.id
+      })
     }
 
     function gotItem(req, res) {
